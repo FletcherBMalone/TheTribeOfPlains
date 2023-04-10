209f8219e7ff509a42fc369d140e16d0f30bbf7f
FletcherBMalone <fletcherbrmalone@gmail.com>
2023-04-10 16:32:51 -0400

    update files

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   experimental: {
-    // Remove the `images` property from your configuration
+    appDir: true,
   },
-  // Add the `output` option to enable static HTML export
-  output: "export",
 };
